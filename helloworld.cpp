170850bcdbc3c17dfbc5503a4d0d07e933276b28
ADARSH-41 <101641489+ADARSH-41@users.noreply.github.com>
2022-03-17 12:09:21 +0530

    Basic Hello World! program in C++.
    Introduces the iostream header file and cout statement to output the input string.

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!\n";  
+	
+	/*'cout' acts as printf() function in C.It is used to print
+	   output on the Console/terminal. */
+	
+   /* '<<' is called insertion operator. You can use multiple 
+	   insertion operators after cout as illustrate below */
+	
+	cout << "Hello" << " World!"; 
+	
+	return 0;
+}
